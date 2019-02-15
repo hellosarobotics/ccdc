8d58b7231690dbb9dbad67e39cabe7b128991252
andrea <hellosarobotics@gmail.com>
2019-02-15 15:39:06 +0100

    First Commit. Maven structure

---
+++ src/main/java/com/sarobotics/ccdc/CcdcApplication.java
@@ -0,0 +1,14 @@
+package com.sarobotics.ccdc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CcdcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CcdcApplication.class, args);
+	}
+
+}
+

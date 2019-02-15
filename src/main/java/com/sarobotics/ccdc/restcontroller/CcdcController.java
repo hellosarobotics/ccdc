eb8d6204765b36e5c2d970f194c180b6df126129
andrea <hellosarobotics@gmail.com>
2019-02-15 16:10:17 +0100

    Added Swagger configuration.

---
+++ src/main/java/com/sarobotics/ccdc/restcontroller/CcdcController.java
@@ -0,0 +1,22 @@
+package com.sarobotics.ccdc.restcontroller;
+
+import static org.springframework.web.bind.annotation.RequestMethod.GET;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import io.swagger.annotations.ApiOperation;
+
+@RestController
+@RequestMapping("/v1")
+public class CcdcController {
+	
+	@CrossOrigin
+	@RequestMapping(value = "/ccdc/{container_nr}", method = GET)
+	@ApiOperation(value = "Validate the container number.")
+	public boolean canCommunicate( @PathVariable( name = "container_nr" ) String containerNr) {
+		return true;
+	}
+
+}
